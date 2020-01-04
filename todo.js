e0c70f1ab78ce4cd928964b4f155bebe3090110f
Melver Dsouza <melverken10@gmail.com>
2020-01-04 13:03:28 +0530

    redo from 20min

--- todo.js
+++ todo.js
@@ -2,13 +2,9 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 const deleteListButton = document.querySelector('[data-delete-list-button]')
-const listDisplayContainer = document.querySelector('[data-list-display-container]')
-const listTitleElement = document.querySelector('[data-list-title]')
-const listTitleElement = document.querySelector('[data-list-count]')
-const tasksContainer = document.querySelector('[data-tasks]')
 
-const LOCAL_STORAGE_LIST_KEY =  'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY =  'task.selectedListId'
+const LOCAL_STORAGE_LIST_KEY ='task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY ='task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
@@ -21,45 +17,35 @@ listsContainer.addEventListener('click', e => {
 
 deleteListButton.addEventListener('click', e => {
     lists = lists.filter(list => list.id !== selectedListId)
-    selectedListId = null
-    saveAndRender()
+    selectedListId =null
 })
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
     const listName = newListInput.value
-    if(listName == '' || listName == null) return 
+    if(listName==null || listName == "") return
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    saveAndRender()
+    render()
 })
 
-function createList (name) {
-    return {id: Date.now().toString(), name: name, tasks: [] }
+function createList(name) {
+    return {id: Date.now.toString(), name: name, tasks: []}
 }
 
-function saveAndRender () {
+function saveAndRender() {
     save()
     render()
 }
 
-function save () {
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
-    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+function save() {
+    localStorage.getItem(JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY,selectedListId)
 }
 
-function render () {
-    clearElement (listsContainer)
-    renderLists()
-    if(selectedListId == null) {
-listDisplayContainer.style.display = 'none'
-    } else {
-listDisplayContainer.style.display = ''
-    }
-}
-
-function renderLists() {
+function render() {
+    clearElement(listsContainer)
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
@@ -72,12 +58,10 @@ function renderLists() {
     })
 }
 
-function clearElement (element) {
+function clearElement(element) {
     while(element.firstChild) {
         element.removeChild(element.firstChild)
     }
 }
 
-
-
-render();
+render()
